5f8fadc2dc3767ffb912d604060fa6e30134a107
Vlad <dv.vlad98@mail.ru>
2017-12-23 05:43:55 +0300

    update

--- src/db.js
+++ src/db.js
@@ -56,11 +56,9 @@ function getStatisticsByCustomDate(datefrom, dateto) {
 }
 
 function newUser(user) {
-    console.log(user.idtypeexercise);
     return db.any(`INSERT INTO public.user(login, password, caloriesnorm, resetcalories, idtypeexercise) VALUES ('${user.login}', '${user.password}', ${user.caloriesnorm}, ${user.resetcalories}, ${user.idtypeexercise})`)
     .then((val) => {
-        console.log(val);
-        return val;
+        return getUserByLogin(user.login);
     });
 }
 
